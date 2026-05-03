c53c1cbddfc5c1452818317a2089af31d5a1ee9b
Felix Guyard <felix.guyard@forensicxlab.com>
2026-05-03 21:07:43 +0200

    Latest

--- install.sh
+++ install.sh
@@ -4,7 +4,10 @@ set -eu
 cargo install --path members/exhume_ntfs  --force
 cargo install --path members/exhume_exfat --force
 cargo install --path members/exhume_extfs --force
+cargo install --path members/exhume_apfs --force
 cargo install --path members/exhume_partitions  --force
 cargo install --path members/exhume_body --force
 cargo install --path members/exhume_lvm --force
 cargo install --path members/exhume_artefacts --force
+cargo install --path members/exhume_indexer --force
+cargo install --path members/exhume_memory --force
